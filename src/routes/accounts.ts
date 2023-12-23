0f117f507aff7c1653b2f19ecb1d241b7f8f3162
mafuchs <mikeafruchs@gmail.com>
2023-12-22 20:56:02 -0600

    do more

--- src/routes/accounts.ts
+++ src/routes/accounts.ts
@@ -29,11 +29,18 @@ accountsRoute.get('/accounts', async (req, res) => {
   const accounts = await client.accountsAndCustomersService.getCustomerAccounts();
   console.log(accounts)
 
-  const positions = await client.balancesAndPositionsService.getPositionsList(accounts[0].account['account-number']);
-  const balance = await client.balancesAndPositionsService.getAccountBalanceValues(accounts[0].account['account-number']);
+  const positions = await client.balancesAndPositionsService.getPositionsList(accounts[0].account['account-number']) as Position[];
+  const balance = await client.balancesAndPositionsService.getAccountBalanceValues(accounts[0].account['account-number']) as Balance;
+
   console.log(positions);
   console.log(balance);
 
+  const positionsFormatted = positions.map( pos => {
+    return { symbol: pos.symbol, underlying: pos['underlying-symbol'], direction: pos['quantity-direction'], quantity: pos.quantity, openPrice: Number(pos['average-open-price'])}
+  });
 
-    //res.render('index', {data: {}});
+  const balanceFormatted = { cash: Number(balance['cash-balance']), netliq: Number(balance['net-liquidating-value'])};
+
+
+  res.json({positions: positionsFormatted, balance: balanceFormatted });
 });
